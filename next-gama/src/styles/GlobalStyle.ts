355617b5ca979ce39e6cf184d2800e2f74adbd30
Jakes Dourado <jakesdourado@MacBook-Pro-de-Jakes.local>
2021-04-03 11:55:02 -0300

    proj add

---
+++ next-gama/src/styles/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+    }
+
+    body{
+        background: #483D8B;
+        font-family: 'Lato', sans-serif;
+        color: #fff;
+    }
+    footer{
+        
+    }
+`
